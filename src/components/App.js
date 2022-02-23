28ac6d1bc8d386cd7d568cb2c1fe25703590535f
Anikita Singhal <92014545+AnikitaSinghal@users.noreply.github.com>
2022-02-23 18:01:59 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
+import List from "./List";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+function App() {
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
+  function delet(id) {
+    let newList = list.filter((valu, index) => {
+      return index != id;
+    });
+    setList(newList);
+  }
+  function edit(id) {
+    list.map((ele, index) => {
+      if (id === index) {
+        setValue(ele);
+      }
+    });
+    delet(id);
+  }
+  return (
+    <div id="main">
+      <textarea
+        id="task"
+        type="text"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      ></textarea>
+      <button
+        id="btn"
+        onClick={() => {
+          if (value === "") {
+          } else {
+            setList((list) => [...list, value]);
+            setValue("");
+          }
+        }}
+      >
+        {" "}
+        Add
+      </button>
+      <div>
+        {list.map((ele, index) => (
+          <List ele={ele} index={index} delete={delet} edits={edit} />
+        ))}
+      </div>
+    </div>
+  );
 }
-
 
 export default App;
